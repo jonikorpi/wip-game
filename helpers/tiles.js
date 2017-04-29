e44b464e3ceac9bf6d8733f9a8240d7e5dfa9f2c
Joni Korpi <me@jonikorpi.com>
2017-04-29 17:56:42 +0300

    Sketches out some tiles, units and effects

--- helpers/tiles.js
+++ helpers/tiles.js
@@ -1,10 +1,49 @@
 export default {
-  water: "transparent",
-  water1: "transparent",
-  water2: "transparent",
-  water3: "transparent",
-  water4: "transparent",
-  ground: "#222",
-  ground2: "#222",
-  desert: "#422",
+  water: {
+    name: "Water",
+    impassable: undefined,
+    sailable: true,
+    difficult: undefined,
+    axes: {
+      water: {
+        max: 10,
+        min: 10,
+        upgrade: undefined,
+        downgrade: "shoal",
+      },
+      heat: {
+        max: 10,
+        min: 10,
+        upgrade: "shoal",
+        downgrade: "ice",
+      },
+    },
+    onChange: undefined,
+    onSpawn: undefined,
+    onDeath: undefined,
+  },
+
+  shoal: {
+    name: "Shoal",
+    impassable: undefined,
+    sailable: true,
+    difficult: true,
+    axes: {
+      water: {
+        max: 10,
+        min: 10,
+        upgrade: "water",
+        downgrade: "ground",
+      },
+      heat: {
+        max: 10,
+        min: 10,
+        upgrade: "ground",
+        downgrade: "ice",
+      },
+    },
+    onChange: undefined,
+    onSpawn: undefined,
+    onDeath: undefined,
+  },
 };
---
+++ helpers/units.js
@@ -0,0 +1,28 @@
+export default {
+  nothing: {},
+
+  monument: {
+    name: "Monument",
+    owner: undefined,
+    canMove: undefined,
+    axes: {
+      durability: {
+        max: 100,
+        min: 500,
+        upgrade: undefined,
+        downgrade: "nothing",
+      },
+      culture: {
+        max: 100,
+        min: 10,
+        upgrade: "greatMonument",
+        downgrade: undefined,
+      },
+    },
+    onChange: undefined,
+    onSpawn: "destroyPlayer",
+    onDeath: undefined,
+    onUse: "respawnPlayer",
+    onTick: undefined,
+  },
+};
