3fd90b4802811e07412eb4e3becdc969e881bda5
Joni Korpi <me@jonikorpi.com>
2017-05-10 12:20:13 +0300

    Evens out reflections

--- components/Reflections.js
+++ components/Reflections.js
@@ -3,26 +3,7 @@ import React, { PureComponent } from "react";
 import hex from "../helpers/hex.js";
 import styles from "../helpers/styles.js";
 
-const multipliers = [
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  1,
-  1,
-  1,
-  1.382,
-  1.618,
-  1.382,
-  1,
-  1,
-  1,
-];
+const multipliers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 export default class Reflections extends PureComponent {
   render() {
@@ -37,7 +18,7 @@ export default class Reflections extends PureComponent {
       >
         {tiles.map(({ left, top, hexPoints }, index) => {
           const reflectionPoints = hexPoints.reduce((result, point, index) => {
-            return `${result} ${point[0]},${point[1] + hex.ridgeHeight + hex.ridgeHeight * 0.618 * multipliers[index]}`;
+            return `${result} ${point[0]},${point[1] + hex.ridgeHeight + hex.ridgeHeight * multipliers[index]}`;
           }, "");
 
           return (
