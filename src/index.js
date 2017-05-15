d873781fe08d3dbcc96c95c912b48ba129e7b259
Joni Korpi <me@jonikorpi.com>
2017-05-16 00:13:58 +0300

    Adds react-addons-perf

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import Game from "./Game";
 import "./reset.css";
 import "./base.css";
 
+const env = (process && process.env && process.env.NODE_ENV) || "development";
+const dev = env === "development";
+
+if (dev) {
+  window.Perf = require("react-addons-perf");
+  window.Perf.start();
+
+  setTimeout(() => {
+    window.Perf.printInclusive();
+    window.Perf.printWasted();
+  }, 5000);
+}
+
 ReactDOM.render(<Game />, document.getElementById("game"));
