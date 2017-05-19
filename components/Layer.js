381dbb2af47e1c10cd3fe58b09a48aeadedd2e48
Joni Korpi <me@jonikorpi.com>
2017-05-19 23:30:42 +0300

    Removes wrappers from Layers

--- components/Layer.js
+++ components/Layer.js
@@ -3,8 +3,17 @@ import React from "react";
 import hex from "../helpers/hex.js";
 
 const Layer = ({ children, className, style, viewBox }) => (
-  <div className={`layer ${className ? className : ""}`} style={style}>
-
+  <svg
+    className={`layer ${className ? className : ""}`}
+    style={style}
+    shapeRendering="optimizeSpeed"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    viewBox={
+      viewBox ||
+        `${-hex.width} ${-hex.height} ${hex.width * 3} ${hex.height * 3}`
+    }
+  >
     <style jsx>{`
       .layer {
         width: 300%;
@@ -16,26 +25,10 @@ const Layer = ({ children, className, style, viewBox }) => (
         {/*-webkit-backface-visibility: hidden;
         backface-visibility: hidden;*/}
       }
-
-      .layerSVG {
-        width: 100%;
-        height: 100%;
-      }
     `}</style>
 
-    <svg
-      className="layerSVG"
-      shapeRendering="optimizeSpeed"
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      viewBox={
-        viewBox ||
-          `${-hex.width} ${-hex.height} ${hex.width * 3} ${hex.height * 3}`
-      }
-    >
-      {children}
-    </svg>
-  </div>
+    {children}
+  </svg>
 );
 
 export default Layer;
