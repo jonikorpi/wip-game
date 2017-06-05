87cf6f96b6de4cea155be67c15a497d5ece91be9
Joni Korpi <me@jonikorpi.com>
2017-06-05 18:54:54 +0300

    Another fix to rotated padding

--- components/Layer.js
+++ components/Layer.js
@@ -4,7 +4,7 @@ import styles from "../helpers/styles.js";
 import hex from "../helpers/hex.js";
 
 const Layer = ({ zOffset = 0, children, heightRatio = 1, rotate = 0 }) => {
-  const x = rotate === 90 ? hex.height : 0;
+  const x = rotate === 90 ? hex.perRegionX / hex.perRegionY * hex.width : 0;
   const y = zOffset
     ? `${(1 - heightRatio) * zOffset / styles.perspective * hex.size}`
     : 0;
